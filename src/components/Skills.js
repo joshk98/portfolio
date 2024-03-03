6bc1528f392adf3914be49653e04f4d5668fcb77
joshkkuhn@gmail.com <joshkkuhn@gmail.com>
2024-03-03 21:48:19 +0000

    New logo for AWS, made Skills code more DRY

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,35 +1,25 @@
 import React from "react";
 import "../styles/skills.css";
 
-const frontData = ["html5", "css3", "javascript", "react", "figma"];
+const frontSkills = ["html5", "css3", "javascript", "react", "figma"];
 
-const backData = [
-  "nodejs",
-  "sequelize",
-  "postgresql",
-  "docker",
-  "amazonwebservices",
-];
+const backSkills = ["nodejs", "sequelize", "postgresql", "docker"];
 
-const otherData = ["bash", "git", "github", "wordpress"];
+const otherSkills = ["bash", "git", "github", "wordpress"];
 
-const mappedFrontData = frontData.map((skill, index) => ({
-  id: index + 1,
-  src: `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${skill}/${skill}-original.svg`,
-  alt: skill,
-}));
+const generateImageSkills = (data) => {
+  return data.map((skill, index) => ({
+    id: index + 1,
+    src: `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${skill}/${skill}-original.svg`,
+    alt: skill,
+  }));
+};
 
-const mappedBackData = backData.map((skill, index) => ({
-  id: index + 1,
-  src: `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${skill}/${skill}-original.svg`,
-  alt: skill,
-}));
-
-const mappedOtherData = otherData.map((skill, index) => ({
-  id: index + 1,
-  src: `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${skill}/${skill}-original.svg`,
-  alt: skill,
-}));
+const skillsMap = {
+  front: generateImageSkills(frontSkills),
+  back: generateImageSkills(backSkills),
+  other: generateImageSkills(otherSkills),
+};
 
 function Skills() {
   return (
@@ -39,7 +29,7 @@ function Skills() {
         <div className="skills-type">
           <h2>Front-End</h2>
           <div className="skills-images">
-            {mappedFrontData.map((skill) => (
+            {skillsMap.front.map((skill) => (
               <div key={skill.id}>
                 <img src={skill.src} alt={skill.alt} />
               </div>
@@ -49,17 +39,21 @@ function Skills() {
         <div className="skills-type">
           <h2>Back-End</h2>
           <div className="skills-images">
-            {mappedBackData.map((skill) => (
+            {skillsMap.back.map((skill) => (
               <div key={skill.id}>
                 <img src={skill.src} alt={skill.alt} />
               </div>
             ))}
+            <img
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/amazonwebservices/amazonwebservices-original-wordmark.svg"
+              alt="amazonwebservices"
+            />
           </div>
         </div>
         <div className="skills-type">
           <h2>Other</h2>
           <div className="skills-images">
-            {mappedOtherData.map((skill) => (
+            {skillsMap.other.map((skill) => (
               <div key={skill.id}>
                 <img src={skill.src} alt={skill.alt} />
               </div>
